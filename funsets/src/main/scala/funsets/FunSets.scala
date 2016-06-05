bab554bb6759d886d4f590c570fe6ac6c76ff870
Paolo Costa <paolo.enrico.costa@gmail.com>
2016-06-05 18:09:45 +0200

    fix

--- funsets/src/main/scala/funsets/FunSets.scala
+++ funsets/src/main/scala/funsets/FunSets.scala
@@ -9,48 +9,45 @@ object FunSets {
    * We represent a set by its characteristic function, i.e.
    * its `contains` predicate.
    */
-  type Pippo = Int => Boolean
+  type Set = Int => Boolean
 
   /**
    * Indicates whether a set contains a given element.
    */
-  def contains(s: Pippo, elem: Int): Boolean = s(elem)
+  def contains(s: Set, elem: Int): Boolean = s(elem)
 
-  val t = Set(8)
-  val t2 = Pippo(8)
-  println(t)
   /**
    * Returns the set of the one given element.
    */
-    def singletonSet(elem: Int): Pippo = x => (x==elem)
-    def all: Pippo = x => true
-    def none: Pippo = x => false
-    def even: Pippo = x => (x % 2 == 0)
-    def odd: Pippo = x => (x % 2 == 1)
+    def singletonSet(elem: Int): Set = x => (x==elem)
+    def all: Set = x => true
+    def none: Set = x => false
+    def even: Set = x => (x % 2 == 0)
+    def odd: Set = x => (x % 2 == 1)
 
   /**
    * Returns the union of the two given sets,
    * the sets of all elements that are in either `s` or `t`.
    */
-    def union(s: Pippo, t: Pippo): Pippo = x => contains(s, x) || contains(t,x)
-  
+    def union(s: Set, t: Set): Set = x => contains(s, x) || contains(t,x)
+
   /**
    * Returns the intersection of the two given sets,
    * the set of all elements that are both in `s` and `t`.
    */
-    def intersect(s: Pippo, t: Pippo): Pippo = x => contains(s, x) && contains(t,x)
-  
+    def intersect(s: Set, t: Set): Set = x => contains(s, x) && contains(t,x)
+
   /**
    * Returns the difference of the two given sets,
    * the set of all elements of `s` that are not in `t`.
    */
-    def diff(s: Pippo, t: Pippo): Pippo = x => contains(s, x) && !contains(t,x)
-  
+    def diff(s: Set, t: Set): Set = x => contains(s, x) && !contains(t,x)
+
   /**
    * Returns the subset of `s` for which `p` holds.
    */
-    def filter(s: Pippo, p: Int => Boolean): Pippo = x => contains(s, x) && p(x)
-  
+    def filter(s: Set, p: Int => Boolean): Set = x => contains(s, x) && p(x)
+
 
   /**
    * The bounds for `forall` and `exists` are +/- 1000.
@@ -60,7 +57,7 @@ object FunSets {
   /**
    * Returns whether all bounded integers within `s` satisfy `p`.
    */
-    def forall(s: Pippo, p: Int => Boolean): Boolean = {
+    def forall(s: Set, p: Int => Boolean): Boolean = {
     def iter(a: Int): Boolean = {
       if (a>1000) true
       else if (s(a) && !p(a)) false
@@ -68,22 +65,22 @@ object FunSets {
     }
     iter(-1000)
   }
-  
+
   /**
    * Returns whether there exists a bounded integer within `s`
    * that satisfies `p`.
    */
-    def exists(s: Pippo, p: Int => Boolean): Boolean = !forall(x=> s(x), x => !p(x))
-  
+    def exists(s: Set, p: Int => Boolean): Boolean = !forall(s, x => !p(x))
+
   /**
    * Returns a set transformed by applying `f` to each element of `s`.
    */
-    def map(s: Pippo, f: Int => Int): Pippo = x=> exists(s, y => f(y) == x)
-  
+    def map(s: Set, f: Int => Int): Set = x=> exists(s, y => f(y) == x)
+
   /**
    * Displays the contents of a set
    */
-  def toString(s: Pippo): String = {
+  def toString(s: Set): String = {
     val xs = for (i <- -bound to bound if contains(s, i)) yield i
     xs.mkString("{", ",", "}")
   }
@@ -91,7 +88,7 @@ object FunSets {
   /**
    * Prints the contents of a set on the console.
    */
-  def printSet(s: Pippo) {
+  def printSet(s: Set) {
     println(toString(s))
   }
 }
